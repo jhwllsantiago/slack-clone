a49bb5c95e025631ffd8f736c93717722973e162
Jhowell Santiago <jhwllsantiago@gmail.com>
2022-10-31 21:05:36 +0800

    added message grouping by date functionality

--- src/components/DisplayMessages/DisplayMessages.jsx
+++ src/components/DisplayMessages/DisplayMessages.jsx
@@ -25,34 +25,50 @@ const DisplayMessages = ({ id, type }) => {
       {isLoading && <img src={loadingGif} alt="" className="loading" />}
       {!isLoading && (
         <ul className="message-list">
-          {messages
-            ?.filter(({ sender, receiver }) => sender.id !== receiver.id)
-            .map(({ id, sender, body, created_at }) => {
-              const checker = sender.id === signed_in;
-              const contact = contacts?.find(
-                (contact) => contact.id === sender.id
-              );
+          {messages?.reduce((nodes, message, idx) => {
+            const { body, created_at, id, sender } = message;
+            const dateExists = nodes.some((node) => {
               return (
-                <li key={id} className="message-item">
-                  {checker && <img src={avatar} alt="" className="avatar" />}
-                  {!checker && (
-                    <div className="letter-img">
-                      {(contact?.name ?? sender.email)
-                        .substring(0, 1)
-                        .toUpperCase()}
-                    </div>
-                  )}
-                  <p className="message-details">
-                    <span className="name">
-                      {checker ? "You" : contact?.name ?? sender.email}
-                    </span>
-                    <span className="time"> {getTime(created_at)}</span>
-                    <span className="date"> {getDate(created_at)}</span>
-                  </p>
-                  <p className="message">{body}</p>
-                </li>
+                node.props.children.props?.children === getDate(created_at)
               );
-            })}
+            });
+            const checker = sender.id === signed_in;
+            const contact = contacts?.find(
+              (contact) => contact.id === sender.id
+            );
+
+            const newNode = (
+              <li key={id} className="message-item">
+                {checker && <img src={avatar} alt="" className="avatar" />}
+                {!checker && (
+                  <div className="letter-img">
+                    {(contact?.name ?? sender.email)
+                      .substring(0, 1)
+                      .toUpperCase()}
+                  </div>
+                )}
+                <p className="message-details">
+                  <span className="name">
+                    {checker ? "You" : contact?.name ?? sender.email}
+                  </span>
+                  <span className="time"> {getTime(created_at)}</span>
+                </p>
+                <p className="message">{body}</p>
+              </li>
+            );
+
+            if (dateExists) {
+              return [...nodes, newNode];
+            }
+
+            return [
+              ...nodes,
+              <li className="date" key={idx}>
+                <span>{getDate(created_at)}</span>
+              </li>,
+              newNode,
+            ];
+          }, [])}
           <div ref={bottomRef} />
         </ul>
       )}
--- src/util/getDate.js
+++ src/util/getDate.js
@@ -1,11 +1,45 @@
 const getDate = (timestamp) => {
-  const date = new Date(timestamp)
-    .toLocaleDateString("en-US", {
-      dateStyle: "medium",
-    })
-    .toUpperCase();
+  const date = new Date(timestamp).toLocaleDateString("en-US", {
+    dateStyle: "full",
+  });
+  const today = new Date().toLocaleDateString("en-US", {
+    dateStyle: "full",
+  });
+  if (date === today) return "Today";
 
-  return date;
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let wd = weekday[new Date(timestamp).getDay()];
+  let m = month[new Date(timestamp).getMonth()];
+  let d = new Date(timestamp).getDate();
+  if (d > 3 && d < 21) d += "th";
+  else if (d % 10 === 1) d += "st";
+  else if (d % 10 === 2) d += "nd";
+  else if (d % 10 === 3) d += "rd";
+  else d += "th";
+
+  return `${wd}, ${m} ${d}`;
 };
 
 export default getDate;
